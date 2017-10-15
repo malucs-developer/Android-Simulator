c0316835cdc2574516a80ab7e99c7833d8472841
Malucs Developer <malucs.developer@gmail.com>
2017-10-16 00:06:28 +0100

    Add DialerActivity

---
+++ app/src/main/java/com/example/android/androidsimulator/DialerActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.androidsimulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DialerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialer);
+    }
+}
